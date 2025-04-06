c30850983d700a214ced25784a9cfd582ecb9a5d
Wind He <g311571057@gmail.com>
2025-04-06 17:18:57 +0800

    wip(login): save token into datastore

--- app/src/main/kotlin/day/vitayuzu/neodb/data/AuthRepository.kt
+++ app/src/main/kotlin/day/vitayuzu/neodb/data/AuthRepository.kt
@@ -1,46 +1,37 @@
 package day.vitayuzu.neodb.data
 
 import android.util.Log
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
-import day.vitayuzu.neodb.data.schema.AuthClientIdentify
 import day.vitayuzu.neodb.util.AUTH_CALLBACK
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.map
 import org.publicvalue.multiplatform.oidc.OpenIdConnectClient
 
 class AuthRepository(
     private val remoteSource: RemoteSource = RemoteSource(),
-    private val dataStore: DataStore<Preferences>
+    private val preferenceSource: LocalPreferenceSource,
 ) {
     suspend fun registerAppIfNeeded(): Result<OpenIdConnectClient> {
-
-        val clientId = getAuthClientId()
-        val clientSecret = getAuthClientSecret()
+        val clientId = preferenceSource.getAuthClientId()
+        val clientSecret = preferenceSource.getAuthClientSecret()
 
         if (clientId != null && clientSecret != null) {
             Log.d("AuthRepository", "Found saved auth client")
             return Result.success(constructAuthClient(clientId, clientSecret))
         } else {
-
             Log.d("AuthRepository", "No saved auth client found, registering...")
             val oauthClientResult = remoteSource.registerOauthAPP()
 
             if (oauthClientResult.isSuccess) {
-                val oauthClientData = oauthClientResult.getOrElse {
-                    return signalRegistrationFailure()
-                }
+                val oauthClientData =
+                    oauthClientResult.getOrElse {
+                        return signalRegistrationFailure()
+                    }
                 try {
-                    saveAuthClientIdentify(oauthClientData)
+                    preferenceSource.saveAuthClientIdentify(oauthClientData)
                     Log.d("AuthRepository", "Saved auth client")
                     return Result.success(
                         constructAuthClient(
                             oauthClientData.clientId,
-                            oauthClientData.clientSecret
-                        )
+                            oauthClientData.clientSecret,
+                        ),
                     )
                 } catch (e: Exception) {
                     return signalRegistrationFailure()
@@ -51,42 +42,25 @@ class AuthRepository(
         }
     }
 
-    private suspend fun saveAuthClientIdentify(identify: AuthClientIdentify) {
-        dataStore.edit {
-            it[CLIENT_ID] = identify.clientId
-            it[CLIENT_SECRET] = identify.clientSecret
+    suspend fun storeAccessToken(token: String) {
+        try {
+            preferenceSource.storeAccessToken(token)
+        } catch (e: Exception) {
+            Log.e("AuthRepository", "Failed to store access token", e)
         }
     }
 
-    private suspend fun getAuthClientId(): String? = dataStore.data
-        .map { it[CLIENT_ID] }
-        .catch {
-            Log.e("AuthRepository", "Error while reading preferences", it)
-            emit(null)
-        }
-        .firstOrNull()
-
-    private suspend fun getAuthClientSecret(): String? = dataStore.data
-        .map { it[CLIENT_SECRET] }
-        .catch {
-            Log.e("AuthRepository", "Error while reading preferences", it)
-            emit(null)
-        }
-        .firstOrNull()
-
     private companion object {
         val AuthRegisterException: Exception = Exception("Failed to register app")
+
         fun <T> signalRegistrationFailure(): Result<T> {
             Log.e("AuthRepository", "Failed to register app")
             return Result.failure(AuthRegisterException)
         }
 
-        val CLIENT_ID = stringPreferencesKey("client_id")
-        val CLIENT_SECRET = stringPreferencesKey("client_secret")
-
         fun constructAuthClient(
             clientId: String,
-            clientSecret: String
+            clientSecret: String,
         ) = OpenIdConnectClient {
             endpoints {
                 tokenEndpoint = "https://neodb.social/oauth/token"
