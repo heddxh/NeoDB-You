8c1687c42feb7593a53783bb76f1639861cd6126
Wind He <g311571057@gmail.com>
2025-04-26 21:51:19 +0800

    feat: top app bar

--- app/src/main/kotlin/day/vitayuzu/neodb/MainActivity.kt
+++ app/src/main/kotlin/day/vitayuzu/neodb/MainActivity.kt
@@ -5,14 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -56,24 +59,32 @@ class MainActivity : ComponentActivity() {
  * Main scaffold for the app.
  * It contains the bottom navigation bar, top appbar and the main content(screens).
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScaffold(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
+    val currentBackStack by navController.currentBackStackEntryAsState()
+    val currentScreen = mainScreens.find { screen ->
+        currentBackStack?.destination?.hierarchy?.any { nav ->
+            nav.hasRoute(screen.route::class)
+        } ?: false
+    }
     Scaffold(
         modifier = modifier,
+        topBar = {
+            if (currentScreen != null) {
+                TopAppBar(
+                    title = { Text(stringResource(currentScreen.name)) },
+                )
+            }
+        },
         bottomBar = {
             NavigationBar {
-                val currentBackStack by navController.currentBackStackEntryAsState()
-                val currentDestination = currentBackStack?.destination
                 mainScreens.forEach { screen ->
                     NavigationBarItem(
-                        icon = {
-                            Icon(screen.icon, contentDescription = screen.name)
-                        },
-                        label = { Text(screen.name) },
-                        selected = currentDestination?.hierarchy?.any {
-                            it.hasRoute(screen.route::class)
-                        } == true,
+                        icon = { Icon(screen.icon, contentDescription = null) },
+                        label = { Text(stringResource(screen.name)) },
+                        selected = currentScreen == screen,
                         onClick = {
                             navController.navigate(screen.route) {
                                 popUpTo(navController.graph.findStartDestination().id) {
--- app/src/main/kotlin/day/vitayuzu/neodb/util/Navi.kt
+++ app/src/main/kotlin/day/vitayuzu/neodb/util/Navi.kt
@@ -1,10 +1,12 @@
 package day.vitayuzu.neodb.util
 
+import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
+import day.vitayuzu.neodb.R
 import kotlinx.serialization.Serializable
 
 /**
@@ -26,9 +28,9 @@ sealed interface Navi {
     companion object {
         // Top level screens
         val mainScreens = listOf(
-            MainScreen("Home", Home, Icons.Default.Home),
-            MainScreen("Library", Library, Icons.Default.DateRange), // FIXME: library icon
-            MainScreen("Settings", Settings, Icons.Default.Settings),
+            MainScreen(R.string.home_title, Home, Icons.Default.Home),
+            MainScreen(R.string.library_title, Library, Icons.Default.DateRange), // FIXME: library icon
+            MainScreen(R.string.settings_title, Settings, Icons.Default.Settings),
         )
     }
 }
@@ -40,7 +42,7 @@ sealed interface Navi {
  * @param icon The icon to display for this screen in the bottom navigation bar.
  */
 data class MainScreen<T : Navi>(
-    val name: String,
+    @StringRes val name: Int,
     val route: T,
     val icon: ImageVector,
 )
