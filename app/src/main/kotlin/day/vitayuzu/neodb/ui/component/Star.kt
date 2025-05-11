f979a5d0903dc68160ec091941ae9c7e08ae7c37
Wind He <g311571057@gmail.com>
2025-05-11 23:26:55 +0800

    fix: star component

--- app/src/main/kotlin/day/vitayuzu/neodb/ui/component/Star.kt
+++ app/src/main/kotlin/day/vitayuzu/neodb/ui/component/Star.kt
@@ -13,29 +13,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import day.vitayuzu.neodb.R
 import day.vitayuzu.neodb.ui.theme.ratingColor
 
 /**
- * @param rating rating scores in 5 system
+ * @param rating rating scores in 10 system
  */
 @Composable
 fun StarsWithScores(
     rating: Float,
     modifier: Modifier = Modifier,
 ) {
+    val ratingInFive = rating.div(2)
     Row(
         modifier = modifier,
         horizontalArrangement = Arrangement.spacedBy(2.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         RatingStars(
-            full = rating.toInt(),
-            half = rating > rating.toInt(),
+            full = ratingInFive.toInt(),
+            half = ratingInFive > ratingInFive.toInt(),
         )
         Text(
-            text = "%.1f".format(rating),
+            text = "%.1f".format(ratingInFive),
             style = MaterialTheme.typography.titleSmall,
             color = MaterialTheme.colorScheme.ratingColor,
         )
@@ -47,6 +49,7 @@ fun RatingStars(
     full: Int,
     half: Boolean,
     modifier: Modifier = Modifier,
+    empty: Int = 5 - full - if (half) 1 else 0,
 ) {
     Row(
         horizontalArrangement = Arrangement.spacedBy(2.dp, Alignment.Start),
@@ -57,7 +60,10 @@ fun RatingStars(
             StarIcon()
         }
         if (half) {
-            StarIcon(isHalf = true)
+            StarIcon(kind = Star.HALF)
+        }
+        repeat(empty) {
+            StarIcon(kind = Star.EMPTY)
         }
     }
 }
@@ -65,21 +71,40 @@ fun RatingStars(
 @Composable
 fun StarIcon(
     modifier: Modifier = Modifier,
-    isHalf: Boolean = false,
+    kind: Star = Star.FULL,
 ) {
-    if (isHalf) {
-        Icon(
-            painterResource(R.drawable.star_half),
-            tint = MaterialTheme.colorScheme.ratingColor,
-            contentDescription = null,
-            modifier = modifier.size(16.dp).offset(y = (-0.5).dp), // Visually center
-        )
-    } else {
-        Icon(
+    when (kind) {
+        Star.FULL -> Icon(
             Icons.Filled.Star,
             tint = MaterialTheme.colorScheme.ratingColor,
             contentDescription = null,
             modifier = modifier.size(16.dp).offset(y = (-0.5).dp), // Visually center
         )
+
+        Star.HALF -> Icon(
+            painterResource(R.drawable.star_half),
+            tint = MaterialTheme.colorScheme.ratingColor,
+            contentDescription = null,
+            modifier = modifier.size(16.dp).offset(y = (-0.5).dp), // Visually center
+        )
+
+        Star.EMPTY -> Icon(
+            painterResource(R.drawable.star_empty),
+            tint = MaterialTheme.colorScheme.ratingColor,
+            contentDescription = null,
+            modifier = modifier.size(16.dp).offset(y = (-0.5).dp), // Visually center
+        )
     }
+}
+
+enum class Star {
+    FULL,
+    HALF,
+    EMPTY,
+}
+
+@Preview
+@Composable
+private fun StarsWithScoresPreview() {
+    StarsWithScores(rating = 6.8f)
 }
