534b6f0c8162c928c345ce9649c9dfdf34612cf2
Wind He <g311571057@gmail.com>
2025-07-08 11:44:46 +0800

    throw and interrupt build when error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,58 +43,48 @@ private fun configureSigning(signingConfigs: NamedDomainObjectContainer<out Sign
         val keyPassword = getEnv("SIGN_KEY_PWD")
         val keyAlias = getEnv("SIGN_KEY_ALIAS")
 
-        if (keyBase64 != null && keyPassword != null && keyAlias != null) {
-            val buildDir = layout.buildDirectory.asFile
-                .get()
-                .also { it.mkdirs() }
-            val tmpFile = File.createTempFile("sign-", ".ks", buildDir)
-            try {
-                tmpFile.writeBytes(Base64.decode(keyBase64))
-                tmpFile.deleteOnExit()
-                signingConfigs.create("release") {
-                    storeFile = tmpFile
-                    storePassword = keyPassword
-                    this.keyAlias = keyAlias
-                    this.keyPassword = keyPassword
-                }
-            } catch (e: Exception) {
-                println(
-                    "Error decoding or writing keystore file: ${e.localizedMessage ?: e.stackTraceToString()}",
-                )
-                tmpFile.delete()
+        requireNotNull(keyBase64) { "Missing SIGN_KEY_BASE64" }
+        requireNotNull(keyPassword) { "Missing SIGN_KEY_PWD" }
+        requireNotNull(keyAlias) { "Missing SIGN_KEY_ALIAS" }
+
+        val buildDir = layout.buildDirectory.asFile
+            .get()
+            .apply { mkdirs() }
+        val tmpFile = File.createTempFile("sign-", ".ks", buildDir)
+        try {
+            tmpFile.writeBytes(Base64.decode(keyBase64))
+            tmpFile.deleteOnExit()
+            signingConfigs.create("release") {
+                storeFile = tmpFile
+                storePassword = keyPassword
+                this.keyAlias = keyAlias
+                this.keyPassword = keyPassword
             }
-        } else {
-            println(
-                "Missing one or more signing env (SIGN_KEY_BASE64, SIGN_KEY_PWD, SIGN_KEY_ALIAS)",
-            )
+        } catch (e: Exception) {
+            tmpFile.delete()
+            throw e
         }
     } else {
         // Read from file
         val keystorePropertiesFile = rootProject.file("keystore.properties")
-        if (keystorePropertiesFile.exists()) {
-            val keystoreProperties =
-                Properties().apply { load(FileInputStream(keystorePropertiesFile)) }
-            val keyAlias = keystoreProperties.getProperty("keyAlias")
-            val keyPassword = keystoreProperties.getProperty("keyPassword")
-            val storeFile = keystoreProperties.getProperty("storeFile")
-            val storePassword = keystoreProperties.getProperty("storePassword")
+        require(keystorePropertiesFile.exists()) { println("Missing keystore.properties") }
+        val keystoreProperties =
+            Properties().apply { load(FileInputStream(keystorePropertiesFile)) }
+        val keyAlias = keystoreProperties.getProperty("keyAlias")
+        val keyPassword = keystoreProperties.getProperty("keyPassword")
+        val storeFile = keystoreProperties.getProperty("storeFile")
+        val storePassword = keystoreProperties.getProperty("storePassword")
 
-            if (keyAlias != null &&
-                keyPassword != null &&
-                storeFile != null &&
-                storePassword != null
-            ) {
-                signingConfigs.create("release") {
-                    this.keyAlias = keyAlias
-                    this.keyPassword = keyPassword
-                    this.storeFile = file(storeFile)
-                    this.storePassword = storePassword
-                }
-            } else {
-                println("One or more properties are missing in keystore.properties.")
-            }
-        } else {
-            println("keystore.properties file not found for local build.")
+        requireNotNull(keyAlias) { "Missing keyAlias in keystore.properties" }
+        requireNotNull(keyPassword) { "Missing keyPassword in keystore.properties" }
+        requireNotNull(storeFile) { "Missing storeFile in keystore.properties" }
+        requireNotNull(storePassword) { "Missing storePassword in keystore.properties" }
+
+        signingConfigs.create("release") {
+            this.keyAlias = keyAlias
+            this.keyPassword = keyPassword
+            this.storeFile = file(storeFile)
+            this.storePassword = storePassword
         }
     }
 }
