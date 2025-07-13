8660e6850c62f2f5a17bf84da1a324e4e844eb42
Wind He <g311571057@gmail.com>
2025-07-13 14:13:31 +0800

    introduce ExpandableText composable

---
+++ app/src/main/kotlin/day/vitayuzu/neodb/ui/component/ExpandableText.kt
@@ -0,0 +1,155 @@
+package day.vitayuzu.neodb.ui.component
+
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.TextAutoSize
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+
+/**
+ * A wrapper version of [Text] composable that can be expanded to show more text.
+ * Optimized reduce recomposition during size animation.
+ *
+ * @param maxLines Control max number of lines when in collapsed state.
+ * @param animationSpec The animation for expanding/collapsing, default is spring.
+ * @see Text
+ */
+@Composable
+fun ExpandableText(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    autoSize: TextAutoSize? = null,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    onTextLayout: ((TextLayoutResult) -> Unit)? = null,
+    style: TextStyle = LocalTextStyle.current,
+    animationSpec: AnimationSpec<Int>? = null,
+) {
+    BoxWithConstraints(modifier = modifier) {
+        val textMeasurer = rememberTextMeasurer()
+
+        val expandedHeightMeasuredSize = remember(style, textMeasurer, this.constraints) {
+            textMeasurer.measure(text, style, constraints = this.constraints)
+        }.size.height
+        val collapsedHeightMeasuredSize = remember(style, textMeasurer, this.constraints) {
+            textMeasurer.measure(text, style, constraints = this.constraints, maxLines = maxLines)
+        }.size.height
+
+        var targetHeight by remember { mutableIntStateOf(collapsedHeightMeasuredSize) }
+
+        var state by remember {
+            mutableStateOf(
+                if (expandedHeightMeasuredSize > collapsedHeightMeasuredSize) {
+                    ExpandableTextState.Collapsed
+                } else {
+                    ExpandableTextState.None
+                },
+            )
+        }
+
+        val animatedHeight by animationSpec?.let {
+            animateIntAsState(targetHeight, animationSpec = it)
+        } ?: animateIntAsState(targetHeight)
+
+        Text(
+            text = text,
+            color = color,
+            autoSize = autoSize,
+            fontSize = fontSize,
+            fontStyle = fontStyle,
+            fontWeight = fontWeight,
+            fontFamily = fontFamily,
+            letterSpacing = letterSpacing,
+            textDecoration = textDecoration,
+            textAlign = textAlign,
+            lineHeight = lineHeight,
+            overflow = overflow,
+            softWrap = softWrap,
+            maxLines = Int.MAX_VALUE, // Constrain text height by Modifier.height
+            minLines = minLines,
+            style = style,
+            onTextLayout = onTextLayout,
+            modifier = Modifier
+                // Deferred state reading to layout phase.
+                .layout { measurable, constraints ->
+                    val placeable =
+                        measurable.measure(constraints.copy(maxHeight = animatedHeight))
+                    layout(placeable.width, animatedHeight) {
+                        placeable.placeRelative(0, 0)
+                    }
+                }.clickable(enabled = state !is ExpandableTextState.None) {
+                    state = when (state) {
+                        ExpandableTextState.Expanded -> {
+                            targetHeight = collapsedHeightMeasuredSize
+                            ExpandableTextState.Collapsed
+                        }
+
+                        ExpandableTextState.Collapsed -> {
+                            targetHeight = expandedHeightMeasuredSize
+                            ExpandableTextState.Expanded
+                        }
+
+                        else -> ExpandableTextState.None
+                    }
+                },
+        )
+    }
+}
+
+private sealed interface ExpandableTextState {
+    object None : ExpandableTextState
+
+    object Expanded : ExpandableTextState
+
+    object Collapsed : ExpandableTextState
+}
+
+@Preview
+@Composable
+private fun PreviewExpandableText() {
+    Column(modifier = Modifier.width(100.dp).height(200.dp)) {
+        ExpandableText("foo")
+        ExpandableText(
+            "foooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo",
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+        )
+    }
+}
