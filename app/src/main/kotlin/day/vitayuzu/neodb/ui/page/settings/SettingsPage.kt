de2e5b92f9f3b689539d093a9bb6f133d5ab2056
Yuzu Vita <g311571057@gmail.com>
2025-08-07 21:45:42 +0800

    basic ui for login button and about card in settings page

--- app/src/main/kotlin/day/vitayuzu/neodb/ui/page/settings/SettingsPage.kt
+++ app/src/main/kotlin/day/vitayuzu/neodb/ui/page/settings/SettingsPage.kt
@@ -1,9 +1,12 @@
 package day.vitayuzu.neodb.ui.page.settings
 
+import android.R.attr.text
 import android.content.Intent
 import android.util.Log
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,10 +19,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -29,10 +36,12 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
@@ -47,6 +56,7 @@ import day.vitayuzu.neodb.ui.theme.NeoDBYouTheme
 fun SettingsPage(modifier: Modifier = Modifier, viewModel: SettingsViewModel = hiltViewModel()) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
+    // Refresh everytime entering this page
     LaunchedEffect(true) {
         viewModel.refresh()
     }
@@ -54,30 +64,46 @@ fun SettingsPage(modifier: Modifier = Modifier, viewModel: SettingsViewModel = h
     val context = LocalContext.current
 
     Surface(modifier = modifier.fillMaxSize()) {
-        if (uiState.isLogin) {
-            UserProfilePart(
-                avatar = uiState.avatar,
-                username = uiState.username,
-                fediAccount = uiState.fediAccount,
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
-                logOut = viewModel::logout,
-            ) {
-                try {
-                    val intent = CustomTabsIntent.Builder().build() // TODO: warmup
-                    intent.launchUrl(context, uiState.url.toUri())
-                } catch (e: Exception) {
-                    Log.e("SettingsPage", "Failed to open ${uiState.url}", e)
+        Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+            if (uiState.isLogin) { // logged in
+                UserProfilePart(
+                    avatar = uiState.avatar,
+                    username = uiState.username,
+                    fediAccount = uiState.fediAccount,
+                    modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                    logOut = viewModel::logout,
+                ) {
+                    try {
+                        val intent = CustomTabsIntent.Builder().build() // TODO: warmup
+                        intent.launchUrl(context, uiState.url.toUri())
+                    } catch (e: Exception) {
+                        Log.e("SettingsPage", "Failed to open ${uiState.url}", e)
+                    }
                 }
+            } else { // need login
+                LoginPart()
             }
-        } else {
-            Box(modifier = Modifier.fillMaxSize()) {
-                Button(
-                    modifier = Modifier.align(Alignment.Center),
-                    onClick = {
-                        context.startActivity(Intent(context, OauthActivity::class.java))
-                    },
-                ) { Text("Log in") }
-            }
+            AboutCard()
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun LoginPart(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Card(
+        modifier = modifier,
+        onClick = { context.startActivity(Intent(context, OauthActivity::class.java)) },
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(painterResource(R.drawable.ic_launcher_foreground), "Logo")
+            Text(
+                "Sign in to enjoy your book, movie, music and other collections on NeoDB.",
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.weight(1f).padding(4.dp),
+            )
+            Icon(Icons.AutoMirrored.Filled.ArrowForward, null, Modifier.padding(end = 8.dp))
         }
     }
 }
@@ -147,6 +173,101 @@ private fun UserProfilePart(
                 Icon(Icons.AutoMirrored.Filled.ExitToApp, null)
                 Spacer(Modifier.size(ButtonDefaults.IconSpacing))
                 Text(stringResource(R.string.settings_account_logOut), softWrap = false)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AboutCard(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Column(modifier = modifier) {
+        Text("About App", Modifier.alpha(.6f).padding(8.dp))
+        // Main Card
+        Card(modifier = Modifier) {
+            AboutField(
+                key = { Text("Version") },
+                value = { Text("1.0.0") },
+                icon = { Icon(Icons.Default.Info, null) },
+                modifier = Modifier.fillMaxWidth(),
+            )
+            AboutField(
+                key = { Text("Author") },
+                value = { Text("@heddxh(Yuzu Vita)") },
+                icon = { Icon(Icons.Default.AccountCircle, null) },
+                modifier = Modifier.fillMaxWidth(),
+            )
+            AboutField(
+                key = { Text("GitHub") },
+                value = {
+                    Icon(
+                        painterResource(R.drawable.outline_arrow_outward_24),
+                        "Go to GitHub repo",
+                        Modifier.size(16.dp),
+                    )
+                },
+                icon = {
+                    Icon(
+                        painterResource(R.drawable.github_mark),
+                        null,
+                        // Editing GitHub trademark is not permitted.
+                        // Add padding to make all icons the same size.
+                        Modifier.padding(2.dp),
+                    )
+                },
+                modifier = Modifier.fillMaxWidth().clickable {
+                    try {
+                        val intent = CustomTabsIntent.Builder().build()
+                        intent.launchUrl(
+                            context,
+                            "https://github.com/heddxh/NeoDB-You".toUri(),
+                        )
+                    } catch (e: Exception) {
+                        Log.e("SettingsPage", "Failed to open $text", e)
+                    }
+                },
+            )
+            AboutField(
+                key = { Text("License") },
+                value = {
+                    Icon(
+                        Icons.AutoMirrored.Filled.ArrowForward,
+                        "See open source license",
+                        Modifier.size(16.dp),
+                    )
+                },
+                icon = { Icon(painterResource(R.drawable.baseline_balance_24), null) },
+                modifier = Modifier.fillMaxWidth().clickable {
+                    // TODO: Open OSS license page.
+                },
+            )
+        }
+    }
+}
+
+@Composable
+fun AboutField(
+    key: @Composable () -> Unit,
+    value: @Composable () -> Unit,
+    icon: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier.padding(vertical = 12.dp, horizontal = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Box(Modifier.size(24.dp), Alignment.Center) {
+            icon()
+        }
+        Spacer(Modifier.width(8.dp))
+        Column {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                key()
+                value()
             }
         }
     }
