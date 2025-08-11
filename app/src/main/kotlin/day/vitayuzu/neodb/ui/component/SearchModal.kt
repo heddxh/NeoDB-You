2e087269d6695cf28d4739c43bb527ee57237466
Yuzu Vita <g311571057@gmail.com>
2025-08-11 23:05:10 +0800

    remove unusable switch for searching with external links
    will implement it later

--- app/src/main/kotlin/day/vitayuzu/neodb/ui/component/SearchModal.kt
+++ app/src/main/kotlin/day/vitayuzu/neodb/ui/component/SearchModal.kt
@@ -1,11 +1,7 @@
 package day.vitayuzu.neodb.ui.component
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.input.clearText
@@ -17,12 +13,10 @@ import androidx.compose.material3.ExpandedFullScreenSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.SearchBarState
 import androidx.compose.material3.SearchBarValue
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopSearchBar
 import androidx.compose.material3.rememberSearchBarState
@@ -37,7 +31,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import day.vitayuzu.neodb.R
 import day.vitayuzu.neodb.ui.model.Entry
 import day.vitayuzu.neodb.ui.theme.NeoDBYouTheme
@@ -120,19 +113,20 @@ fun SearchModal(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxWidth(),
         ) {
-            stickyHeader {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier
-                        .background(MaterialTheme.colorScheme.background)
-                        .padding(8.dp)
-                        .fillMaxWidth(),
-                ) {
-                    Text("Press to fetch from external website")
-                    Switch(checked = false, onCheckedChange = {})
-                }
-            }
+            // TODO: Search with external links
+//            stickyHeader {
+//                Row(
+//                    verticalAlignment = Alignment.CenterVertically,
+//                    horizontalArrangement = Arrangement.SpaceBetween,
+//                    modifier = Modifier
+//                        .background(MaterialTheme.colorScheme.background)
+//                        .padding(8.dp)
+//                        .fillMaxWidth(),
+//                ) {
+//                    Text("Press to fetch from external website")
+//                    Switch(checked = false, onCheckedChange = {})
+//                }
+//            }
             items(resultList.toList(), key = { it.uuid }) {
                 EntryMarkCard(entry = it, mark = null) { type, uuid ->
                     onClickEntry(type, uuid)
