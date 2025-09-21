35214fdf52303105da69a28dd1f2501498bfa636
Yuzu Vita <g311571057@gmail.com>
2025-09-21 23:21:14 +0800

    feat: move selected chips when filtering entry type

--- app/src/main/kotlin/day/vitayuzu/neodb/ui/component/EntryTypeFilterChipsRow.kt
+++ app/src/main/kotlin/day/vitayuzu/neodb/ui/component/EntryTypeFilterChipsRow.kt
@@ -5,12 +5,14 @@ import androidx.compose.animation.expandHorizontally
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkHorizontally
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.FilledIconButton
@@ -21,6 +23,7 @@ import androidx.compose.material3.LocalMinimumInteractiveComponentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -28,8 +31,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import day.vitayuzu.neodb.util.EntryType
+import kotlinx.coroutines.launch
 
-// TODO: Move selected chips to front
 @Composable
 fun EntryTypeFilterChipsRow(
     modifier: Modifier = Modifier,
@@ -37,6 +40,9 @@ fun EntryTypeFilterChipsRow(
     onClick: (EntryType) -> Unit = {},
     onClearFilter: () -> Unit = {},
 ) {
+    val state = rememberLazyListState()
+    val scope = rememberCoroutineScope()
+
     Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
         AnimatedVisibility(
             visible = selectedEntryTypes.isNotEmpty(),
@@ -47,22 +53,34 @@ fun EntryTypeFilterChipsRow(
             CompositionLocalProvider(LocalMinimumInteractiveComponentSize provides Dp.Unspecified) {
                 FilledIconButton(
                     modifier = Modifier.padding(start = 8.dp),
-                    onClick = onClearFilter,
                     shape = FilterChipDefaults.shape,
+                    onClick = {
+                        scope.launch { state.animateScrollToItem(0) }
+                        onClearFilter()
+                    },
                 ) {
                     Icon(Icons.Filled.Clear, "clear")
                 }
             }
         }
-        Row(
-            modifier = Modifier.horizontalScroll(rememberScrollState()).padding(horizontal = 8.dp),
+        LazyRow(
+            state = state,
+            contentPadding = PaddingValues(horizontal = 8.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-            for (type in EntryType.entries) {
+            val types = buildList {
+                addAll(selectedEntryTypes)
+                addAll(EntryType.entries - selectedEntryTypes)
+            }
+            items(types, key = { it.name }) { type ->
                 FilterChip(
+                    modifier = Modifier.animateItem(),
                     label = { Text(stringResource(type.toR())) },
                     selected = type in selectedEntryTypes,
-                    onClick = { onClick(type) },
+                    onClick = {
+                        scope.launch { state.animateScrollToItem(0) }
+                        onClick(type)
+                    },
                 )
             }
         }
