2d3fb2a2c8eba8d7a220ccc8ccd6622ea0d53f7c
Yuzu Vita <vita.yuzupon@gmail.com>
2026-06-24 16:29:32 +0800

    build: release 1.1.0-rc4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = android.namespace
         targetSdk = android.compileSdk
         minSdk = 24
-        versionCode = 12 // TODO: Verify increment before release
-        versionName = "1.1.0-rc3"
+        versionCode = 13 // TODO: Verify increment before release
+        versionName = "1.1.0-rc4"
 
         manifestPlaceholders["auth"] = namespace.toString()
         manifestPlaceholders["app_name"] = "NeoDB You"
