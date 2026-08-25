bbb2b6d840b3f806495268a3d3a7f8bc1b582ba4
Yuzu Vita <vita.yuzupon@gmail.com>
2026-08-25 23:28:56 +0800

    Revert "fix(ui): ensure readable library status labels"
    This reverts commit 1df9205d996863cd4d128060dd55543a43142084.

--- app/src/main/kotlin/day/vitayuzu/neodb/ui/page/library/LibraryPage.kt
+++ app/src/main/kotlin/day/vitayuzu/neodb/ui/page/library/LibraryPage.kt
@@ -21,7 +21,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -106,7 +105,7 @@ private fun LibraryContent(
                         colors = { index ->
                             ToggleButtonDefaults.tonalToggleButtonColors(
                                 checkedContainerColor = color[index],
-                                checkedContentColor = color[index].contrastingContentColor(),
+                                checkedContentColor = Color.White,
                             )
                         },
                         optionContent = { index ->
@@ -144,10 +143,6 @@ private fun LibraryContent(
         }
     }
 }
-
-// Black and white have equal WCAG contrast at this luminance.
-private fun Color.contrastingContentColor(): Color =
-    if (luminance() > 0.179f) Color.Black else Color.White
 
 @Preview
 @Composable
